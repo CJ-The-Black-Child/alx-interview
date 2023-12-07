e7805f0ff4db305d48e2d20a4c8f53c01b190d4d
CJ-The-Black-Child <congomigue@gmail.com>
2023-12-07 18:32:43 +0300

    fixed pycodestyle #pep8 syntaxing and styling

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,18 +10,18 @@ def canUnlockAll(boxes):
     """
     if not boxes:
         return False
-    
+
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
-    
+
     stack = [0]
-    
+
     while stack:
         box = stack.pop()
         for key in boxes[box]:
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 stack.append(key)
-                
+
     return all(unlocked)
